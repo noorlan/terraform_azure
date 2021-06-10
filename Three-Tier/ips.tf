45a2bc795fa755dfbcb6c624f7e0a7c5f4d5abb0
noorlan <kozuev@gmail.com>
2021-06-10 22:06:29 +0000

    added vm

--- Three-Tier/ips.tf
+++ Three-Tier/ips.tf
@@ -18,3 +18,42 @@ resource "azurerm_public_ip" "IP3" {
   location            = azurerm_resource_group.example.location
   allocation_method   = "Dynamic"
 }
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "public1"
+    subnet_id                     = azurerm_subnet.private1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.IP1.id
+  }
+}
+
+resource "azurerm_network_interface" "nic2" {
+  name                = "nic2"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "public2"
+    subnet_id                     = azurerm_subnet.private2.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.IP2.id
+  }
+}
+
+resource "azurerm_network_interface" "nic3" {
+  name                = "nic3"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "public1"
+    subnet_id                     = azurerm_subnet.private3.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.IP3.id
+  }
+}
