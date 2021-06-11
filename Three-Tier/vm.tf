061539bc97ff2be5e9cdc76c8f1781310c35a9ea
noorlan <kozuev@gmail.com>
2021-06-11 11:08:17 +0000

    added scale set

--- Three-Tier/vm.tf
+++ Three-Tier/vm.tf
@@ -26,31 +26,3 @@ resource "azurerm_virtual_machine" "main" {
     disable_password_authentication = false
   }
 }
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "vm1"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.nic1.id]
-  vm_size               = "Standard_DS1_v2"
-  
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path = ""
-      key_data = ("")
-    }
-  }
-}
